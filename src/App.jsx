70d249bcc30db0a9f327f8d9371b47e1729f3340
Sergio Salazar Williams <sawsdevx8@gmail.com>
2024-04-24 14:04:34 -0500

    Add Blog list frontend step 2
    Adjust user name when logged in user

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,13 @@ const App = () => {
     )  
   }, [])
 
+  useEffect(() => {
+    const loggedInUser = JSON.parse(window.localStorage.getItem('loggedInUser'))
+    loggedInUser
+    ? setUser(loggedInUser)
+    : console.log("no user");
+  }, [])
+
   /**
    * Handlers
    */
@@ -25,6 +32,9 @@ const App = () => {
     const user = await loginService.login({
       username, password
     })
+
+
+    window.localStorage.setItem('loggedInUser', JSON.stringify(user))
     setUser(user)
     setUsername('')
     setPassword('')
@@ -55,6 +65,7 @@ const App = () => {
 
   const blogListArea = () => (
     <div>
+        <p>{user.name} logged in</p>
         <h2>blogs</h2>
         {blogs.map(blog =>
           <Blog key={blog.id} blog={blog} />
